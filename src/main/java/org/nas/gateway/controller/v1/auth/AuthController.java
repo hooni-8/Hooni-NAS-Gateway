0f5d9cea06181ebaa9d1a29f20fbdae679291eb1
LeeSH <dltkdgns0726@naver.com>
2025-12-18 03:18:51 +0900

    login log 삭제

--- src/main/java/org/nas/gateway/controller/v1/auth/AuthController.java
+++ src/main/java/org/nas/gateway/controller/v1/auth/AuthController.java
@@ -68,7 +68,7 @@ public class AuthController {
                             .sameSite("Strict")
                             .maxAge(60 * 60 * 24 * 7) // 7일 예시
                             .build();
-                    log.info(accessToken.toString());
+
                     return ResponseEntity.ok()
                             .header(HttpHeaders.SET_COOKIE, accessToken.toString())           // AccessToken Cookie에 저장
                             .body(AuthResponse.getSuccess());
--- src/main/java/org/nas/gateway/service/v1/auth/AuthService.java
+++ src/main/java/org/nas/gateway/service/v1/auth/AuthService.java
@@ -59,7 +59,6 @@ public class AuthService {
     // 로그인 토큰 발급
     private Mono<AuthResponse> handleLogin(UserDetail user, String rawPassword) {
         if (matches(rawPassword, user.getUserPw())) {
-            log.info("matches success");
             String accessToken = jwtTokenProvider.generateAccessToken(user);
             String refreshToken = jwtTokenProvider.generateRefreshToken(user);
 
@@ -68,7 +67,6 @@ public class AuthService {
 
             return Mono.just(AuthResponse.getLoginSuccess(accessToken, refreshToken));
         }
-        log.info("matches fail");
         return Mono.just(AuthResponse.getLoginFail());
     }
 
